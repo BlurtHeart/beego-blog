8e526f59bb7a287119bfadb6ea1bcd335acc5db3
blurtheart <fuhongbofhb@163.com>
2017-07-18 14:00:36 +0800

    implement role model

--- models/rolemodel.go
+++ models/rolemodel.go
@@ -24,6 +24,42 @@ func FindRoleByName(rolename string) Role {
 	return role
 }
 
+func FindRoleById(id int) Role {
+	o := orm.NewOrm()
+	var role Role
+	o.QueryTable(role).Filter("Id", id).One(&role)
+	return role
+}
+
+func SaveRole(role *Role) int64 {
+	o := orm.NewOrm()
+	id, _ := o.Insert(role)
+	return id
+}
+
+func UpdateRole(role *Role) {
+	o := orm.NewOrm()
+	o.Update(role, "RoleName", "Permissions")
+}
+
+func DeleteRole(role *Role) {
+	o := orm.NewOrm()
+	o.Delete(role)
+}
+
+func DeleteRoleById(id int) {
+	o := orm.NewOrm()
+	o.Raw("delete from roles where id=?", id).Exec()
+}
+
+func FindRoles() []*Role {
+	o := orm.NewOrm()
+	var role Role
+	var roles []*Role
+	o.QueryTable(role).All(&roles)
+	return roles
+}
+
 func InsertRoles() {
 	roles := []Role{
 		Role{RoleName: "User", Permissions: FOLLOW | COMMENT | WRITE_ARTICLES},
